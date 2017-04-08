97d6459ae1ad4091b398ed8d50e0aa685975c0af
jialu tu <jialu tu>
2017-04-08 13:07:21 +0100

    .

---
+++ main.py
@@ -0,0 +1,107 @@
+from urllib import request
+import json
+#"team-16|996141146944271"
+default_api = "https://app-gateway.hackathon.ixaris.com/api"
+
+def get_token(programmeID="996141146944271", credentialCode="team-16", password="1No2EXk&"):
+  data = {
+    "programmeId": programmeID,
+    "credentialCode": credentialCode,
+    "password": password
+  }
+
+  header = {
+    "Content-Type": "application/json",
+    "X-callref": password,
+    "X-programmeKey": "{}|{}".format(credentialCode, programmeID),
+  }
+
+  req = request.Request(default_api + "/auth/login", data=str(data).encode('utf-8'), headers=header, method='POST')
+  res = request.urlopen(req, timeout=5)
+
+  token = json.loads(res.read().decode('utf-8'))
+
+  header.update({"authorization": token['token']})
+
+  with open('key.json', 'w') as f:
+    f.write(str(header))
+
+  return header
+
+def issue_card():
+  with open('key.json', 'r') as f:
+    header = f.read()
+    header = json.loads(header.replace("'", '"'))
+
+  data = {
+    "profileId": "97593089101269248",
+    "ownerId": "87593085012710413",
+    "friendlyName": "My third card",
+    "currency": "GBP",
+    "issuingProvider": "IssuingProvider",
+    "processingProvider": "ProcessingProvider",
+    "nameOnCard": "Jane Doe"
+  }
+
+  req = request.Request(default_api + "/managed_cards/_/create", data=str(data).encode('utf-8'), headers=header, method='POST')
+  res = request.urlopen(req, timeout=5)
+
+  print(res.read())
+
+def get_cards():
+  with open('key.json', 'r') as f:
+    header = f.read()
+    header = json.loads(header.replace("'", '"'))
+
+  data = {}
+
+  req = request.Request(default_api + "/managed_cards/get", data=str(data).encode('utf-8'), headers=header, method='POST')
+  res = request.urlopen(req, timeout=5)
+
+  results = res.read()
+
+  results = json.loads(results.decode('utf-8'))
+
+  return results
+
+def create_corporation(name, support_email, notification_email):
+  with open('key.json', 'r') as f:
+    header = f.read()
+    header = json.loads(header.replace("'", '"'))
+
+  data = {
+    "name": name,
+    "profileId": "97593089101268736",
+    "rootCredentialCode": "string",
+    "supportEmail": support_email,
+    "notificationEmail": notification_email,
+    "adminTitle": "Mr",
+    "adminName": "string",
+    "adminSurname": "string",
+    "adminEmail": "string"
+  }
+
+  req = request.Request(default_api + "/corporate_identities/_/create", data=str(data).encode('utf-8'), headers=header, method='POST')
+  res = request.urlopen(req, timeout=5)
+
+  print(res.read())
+
+def get_corporations():
+  with open('key.json', 'r') as f:
+    header = f.read()
+    header = json.loads(header.replace("'", '"'))
+
+  data = {}
+
+  req = request.Request(default_api + "/corporate_identities/get", data=str(data).encode('utf-8'), headers=header, method='POST')
+  res = request.urlopen(req, timeout=5)
+
+  results = res.read()
+
+  results = json.loads(results.decode('utf-8'))
+
+  return results['identities']
+
+if __name__ == '__main__':
+  a = get_corporations()
+  [print(i) for i in a if i['state'] == 'ACTIVE']
