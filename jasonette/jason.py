3e4428fd4455bd378fa34a07e7d5924e7c3fe1da
jialu tu <jialu tu>
2017-04-08 21:57:33 +0100

    .

---
+++ jasonette/jason.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/cards")
+def cards():
+  with open('cards.json', 'r') as f:
+    data = f.read()
+
+  return data
+
+@app.route("/corporations")
+def corporations():
+  with open('corporations.json', 'r') as f:
+    data = f.read()
+
+  return data
+
+if __name__ == "__main__":
+  app.run()
